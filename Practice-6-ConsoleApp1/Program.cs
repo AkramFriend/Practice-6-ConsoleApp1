b3b7dce6340cc5313f16ec2642351ee7eb16bb81
shoevakramchik@gmail.com <shoevakramchik@gmail.com>
2024-07-03 01:02:13 +0300

    Добавьте файлы проекта.

---
+++ Practice-6-ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_6_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("practice-6", true, Encoding.Unicode))
+            {
+                char key = 'д';
+                while (char.ToLower(key) == 'д')
+                {
+                    string note = string.Empty;
+                    Console.Write("ID: ");
+                    note += $"{Console.ReadLine()}\t";
+                    string now = DateTime.Now.ToShortTimeString();
+                    Console.WriteLine($"Время заметки: {now}");
+                    note += $"{now}\t";
+                    Console.Write("ФИО: ");
+                    note += $"{Console.ReadLine()}\t";
+                    Console.Write("Возраст: ");
+                    note += $"{Console.ReadLine()}\t";
+                    Console.Write("Рост: ");
+                    note += $"{Console.ReadLine()}\t";
+                    Console.Write("Дата рождения: ");
+                    note += $"{Console.ReadLine()}\t";
+                    Console.Write("Место рождения: ");
+                    note += $"{Console.ReadLine()}\t"; 
+                    sw.WriteLine(note);
+                    Console.Write("Продолжить д/н: "); key = Console.ReadKey(true).KeyChar;
+                }
+            }
+        }
+    }
+}
